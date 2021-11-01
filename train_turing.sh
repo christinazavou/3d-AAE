10f16054e74dd984368a9bafe4ce33ec2dc00716
christina <czavou01@gmail.com>
2021-11-01 15:50:35 +0200

    run on turing

--- train_turing.sh
+++ train_turing.sh
@@ -9,12 +9,13 @@
 
 SOURCE_DIR=${SOURCE_DIR:-/home/czavou01/3d-AAE}
 PY_EXE=${PY_EXE:-/home/czavou01/miniconda3/envs/decorgan/bin/python}
-CONFIG=${CONFIG:-/home/czavou01/3d-AAE/settings/buildnet/aae/turing/hyperparams.json}
+CONFIG=${CONFIG:-buildnet/aae/turing/hyperparams.json}
 GPU=${GPU:-0}
 MAIN_FILE=${MAIN_FILE:-train_aae.py}
 
 export CUDA_VISIBLE_DEVICES=${GPU}
-
+export PYTHONPATH=$PYTHONPATH:${SOURCE_DIR}:${SOURCE_DIR}/experiments
 cd ${SOURCE_DIR}/experiments
-echo "start ${PY_EXE} ${MAIN_FILE} --config ${CONFIG} --gpu ${GPU}"
-${PY_EXE} ${MAIN_FILE} --config ${CONFIG} --gpu ${GPU}
+args="--config ${SOURCE_DIR}/settings/${CONFIG}"
+echo "start ${PY_EXE} ${MAIN_FILE} $args"
+${PY_EXE} ${MAIN_FILE} $args
